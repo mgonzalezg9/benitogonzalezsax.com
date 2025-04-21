7a06da4fafe9b32808c307dd2859340b999f0691
Manuel Gonzalez Garcia <mgonzalez4648@gmail.com>
2025-04-21 12:13:15 +0200

    Change website colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#1a202c',
+        primary: '#466787',
         secondary: '#4a5568',
-        accent: '#f59e0b',
+        accent: '#e5c029',
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
