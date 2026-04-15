e280a1c6f36089a08de46cb87f24c2ea1a5fef71
Manuel Gonzalez Garcia <mgonzalez4648@gmail.com>
2026-04-15 10:21:00 +0200

    Avoid slugify repetition

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -1,10 +1,4 @@
-const slugify = (value: string) =>
-  value
-    .toLowerCase()
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-+|-+$/g, '');
+import { slugify } from '../lib/typography';
 
 export interface LocationEntry {
   slug: string;
